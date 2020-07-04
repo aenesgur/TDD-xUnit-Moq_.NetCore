a479a832baa143ae67fa7f0d1e8ad5a650c08925
aenesgur <47754791+aenesgur@users.noreply.github.com>
2020-07-04 21:45:11 +0300

    UI layer and theire controller's tests added

---
+++ StationaryStore.UI/Controllers/Admin/ProductController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StationaryStore.DAL.Abstractions;
+using StationaryStore.Entities;
+
+namespace StationaryStore.UI.Controllers.Admin
+{
+    [Route("admin/[controller]/[action]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllAsync();
+            return View(products);
+        }
+
+        // GET: Product/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var isCreated = await _productService.CreateAsync(model);
+            if (isCreated)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
+        }
+
+        // GET: Product/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, Product model)
+        {
+            if (!id.Equals(model.Id))
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            
+            var isUpdated = await _productService.UpdateAsync(id, model);
+            if (isUpdated)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+
+        // GET: Product/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var isDeleted = await _productService.DeleteAsync(id);
+            if (isDeleted)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Tests/StationaryStore.UI.Test/Admin/ProductControllerTest.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StationaryStore.DAL.Abstractions;
+using StationaryStore.Entities;
+using StationaryStore.UI.Controllers.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace StationaryStore.UI.Test.Admin
+{
+    public class ProductControllerTest
+    {
+        private readonly Mock<IProductService> _mock;
+        private readonly ProductController _controller;
+        private readonly List<Product> _products;
+
+        public ProductControllerTest()
+        {
+            _mock = new Mock<IProductService>();
+            _controller = new ProductController(_mock.Object);
+
+            _products = new List<Product>()
+            {
+                new Product
+                {
+                    Id="5ef9e23e1c4d3f78c4e72c1f",
+                    Name="Pen",
+                    IsStock=true,
+                    Price=25
+                },
+                new Product
+                {
+                    Id = "5ef9e23e1c4d3f78c4e72c1e",
+                    Name = "Eraser",
+                    IsStock = false,
+                    Price = 5
+                }
+            };
+        }
+
+        [Fact]
+        public async void Index_ActionExecute_ReturnProductList()
+        {
+            _mock.Setup(x => x.GetAllAsync()).ReturnsAsync(_products);
+            var result = await _controller.Index();
+
+            var resultType = Assert.IsType<ViewResult>(result);
+            var resultModel = Assert.IsAssignableFrom<List<Product>>(resultType.Model);
+            Assert.Equal(2, resultModel.Count);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        public async void Details_IdInValid_ReturnNotFound(string productId)
+        {
+            Product product = null;
+            _mock.Setup(x => x.GetByIdAsync(productId)).ReturnsAsync(product);
+            var result = await _controller.Details(productId);
+
+            var resultType = Assert.IsType<NotFoundResult>(result);
+            Assert.Equal(404, resultType.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("5ef9e23e1c4d3f78c4e72c1f")]
+        public async void Details_ActionExecute_ReturnProduct(string productId)
+        {
+            var product = _products.First();
+            _mock.Setup(x => x.GetByIdAsync(productId)).ReturnsAsync(product);
+            var result = await _controller.Details(productId);
+
+            var resultType = Assert.IsType<ViewResult>(result);
+            var resultModel = Assert.IsAssignableFrom<Product>(resultType.Model);
+        }
+
+        [Fact]
+        public void Create_ActionResult_ReturnView()
+        {
+            var result = _controller.Create();
+
+            var resultType = Assert.IsType<ViewResult>(result);
+        }
+
+        [Theory]
+        [InlineData("5ef9e23e1c4d3f78c4e72c1f")]
+        public async void CreatePOST_InValidModelState_ReturnProduct(string productId)
+        {
+            var product = _products.First(x=>x.Id.Equals(productId));
+            _controller.ModelState.AddModelError("Name", "Name field is required");
+            var result = await _controller.Create(product);
+
+            var resultType = Assert.IsType<ViewResult>(result);
+        }
+
+        [Theory]
+        [InlineData("5ef9e23e1c4d3f78c4e72c1f")]
+        public async void CreatePOST_MethodReturnFalse_ReturnNotFound(string productId)
+        {
+            var product = _products.First(x => x.Id.Equals(productId));
+            _mock.Setup(x => x.CreateAsync(product)).ReturnsAsync(false);
+            var result = await _controller.Create(product);
+
+            var resultType = Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Theory]
+        [InlineData("5ef9e23e1c4d3f78c4e72c1f")]
+        public async void CreatePOST_ActionExecute_RedirectToIndexAction(string productId)
+        {
+            var product = _products.First(x => x.Id.Equals(productId));
+            _mock.Setup(x => x.CreateAsync(product)).ReturnsAsync(true);
+            var result = await _controller.Create(product);
+
+            var resultType = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", resultType.ActionName);
+        }
+    }
+}
